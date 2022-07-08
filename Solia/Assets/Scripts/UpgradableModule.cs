ec2fd7dec73a4f87f5aa2ab32264adc217db5de2
Nano <origamimania@free.fr>
2022-07-08 16:27:05 +0200

    Added Upgradable
    Can now upgrade objects.
    Feature is not quite done, but works
    (doesn't check if inventory has enough resources)

---
+++ Solia/Assets/Scripts/UpgradableModule.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradableModule : MonoBehaviour
+{
+    [Tooltip("What object is spawned when this building is upgraded")]
+    [SerializeField] private GameObject UpgradedObject;
+
+    [Tooltip("The resources needed to upgrade this building")]
+    [SerializeField] private List<Item> NeededItems = new List<Item>();
+
+    [Tooltip("If this building is destroyed when upgraded. ")]
+    [SerializeField] private bool IsReusable = true;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    //function that tries to upgrade the current building with the materials in the inventory
+    public void TryUpgrade(Inventory inventory)
+    {
+        //TODO - check the inventory equals the needed and remove them from the inventory
+
+        //upgrade the building
+        //FIRST, spawn the upgraded version on the same position
+        GameObject.Instantiate(UpgradedObject, transform.position, Quaternion.identity);
+
+        //Debug message on succesful upgrade
+        Debug.Log("Upgrade succesful !");
+
+        //SECOND, destroy myself if not reusable
+        if(!IsReusable)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Solia/Assets/Scripts/Upgrader.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//class that defines an entity allowed to upgrade buildings
+public class Upgrader : MonoBehaviour
+{
+
+    [Tooltip("The range of the upgrader")]
+    [SerializeField] private int upgradeRange;
+
+    [Tooltip("The inventory to use when trying to upgrade")]
+    [SerializeField] private Inventory upgradeInventory;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //subscribe to the upgrade action
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //check if upgrade key is pressed
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //draw the range of the upgrader
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, upgradeRange);
+    }
+
+    public void TryUpgradeClosest(InputAction.CallbackContext context)
+    {
+        //check if key is pressed (otherwise, trigger 3 times : pressed, hold, let go)
+        if(context.performed)
+        {
+            Debug.Log("Tried upgrading closest");
+            //search if there is an upgradable in the range
+            UpgradableModule[] upgradables = FindObjectsOfType<UpgradableModule>();
+
+            UpgradableModule closest = null;
+            float minDistance = Mathf.Infinity;
+            foreach(UpgradableModule upgradableModule in upgradables)
+            {
+                float curDistance = Vector2.Distance(upgradableModule.transform.position, transform.position);
+                //check if in range
+                if(!( curDistance <= upgradeRange ))
+                {
+                    continue;
+                }
+
+                //check if first in range
+                //check if closer than closest
+                if(closest == null || minDistance > curDistance)
+                {
+                    closest = upgradableModule;
+                    minDistance = curDistance;
+                }
+            }
+            //closest still null means no upgradable in range
+            if(closest == null)
+            {
+                return;
+            }
+
+            //call the upgrade function on the upgradable with my inventory and try to upgrade
+            closest.TryUpgrade(upgradeInventory);
+        }
+    }
+}
