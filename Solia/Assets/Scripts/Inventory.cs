627f4fb859e42b1cecb391cce81d5d465652ab62
Lorelei Augeraud <lorelei.augeraud@u-psud.fr>
2022-07-11 19:11:13 +0200

    inventory

--- Solia/Assets/Scripts/Inventory.cs
+++ Solia/Assets/Scripts/Inventory.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
 
     [Tooltip("The list of items in this inventory")]
-    public List<Item> inventory;
+    public List<Tuple<Item,int>> inventory;
 
     // Start is called before the first frame update
     private void Start()
@@ -17,5 +20,42 @@ public class Inventory : MonoBehaviour
     private void Update()
     {
 
+    }
+
+    public void addItem(Item i)
+    {
+        foreach (var iTuples in inventory)
+        {
+            if ((i.name == iTuples.Item1.name) && iTuples.Item2 < 99 )
+            {
+                iTuples.Item2 += 1;
+            }
+            else
+            {
+                iTuples.Item1 = i;
+                iTuples.Item2 = 1;
+            }
+        }
+    }
+    
+    public void supprItem(Item)
+    {
+        foreach (var iTuples in inventory)
+        {
+            if ((Item.name == iTuples.Item1.name) && iTuples.Item2 > 0)
+            {
+                iTuples.Item2 -= 1;
+            }
+        }
+    }
+
+    public void moveItem(Item)
+    {
+        
+    }
+
+    public void fuseItem(Item)
+    {
+        
     }
 }
