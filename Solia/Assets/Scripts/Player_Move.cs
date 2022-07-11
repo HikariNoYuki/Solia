4666e73cd196c053fc86ec671302f7ba71f0300a
Nano <origamimania@free.fr>
2022-07-11 19:48:28 +0200

    Tweaks action map

--- Solia/Assets/Scripts/Player_Move.cs
+++ Solia/Assets/Scripts/Player_Move.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,18 +5,18 @@ public class Player_Move : MonoBehaviour
 {
     // Start is called before the first frame update
     private Vector2 moveDirection; //movement direction asked by the player for the next frame
+
     private Vector2 actualMoveDirection = Vector2.zero; //real value with acceleration, tend to move direction
-    
+
     [Tooltip("Controller associated with player character")]
     [SerializeField] private CharacterController characterController;
 
-    [Tooltip("Character's Move Speed")] 
+    [Tooltip("Character's Move Speed")]
     [SerializeField] private float characterMoveSpeed = 7;
 
-    [Tooltip("Acceleration's percentage per frame")] 
-    [SerializeField] private float accelerationPercent = 0.17f;    
-    
-    
+    [Tooltip("Acceleration's percentage per frame")]
+    [SerializeField] private float accelerationPercent = 0.17f;
+
     private void Start()
     {
     }
@@ -30,19 +29,19 @@ public class Player_Move : MonoBehaviour
     public void Movement(InputAction.CallbackContext callbackContext)
     {
         Debug.Log(callbackContext.phase);
-        if (callbackContext.performed)
+        if(callbackContext.performed)
         {
             moveDirection = callbackContext.ReadValue<Vector2>(); //calculation
         }
-        else if (callbackContext.canceled)
+        else if(callbackContext.canceled)
         {
             moveDirection = Vector2.zero; //Stop movement
         }
     }
-    
+
     public void FixedUpdate()
     {
-        actualMoveDirection += accelerationPercent * (moveDirection - actualMoveDirection);
+        actualMoveDirection += accelerationPercent * ( moveDirection - actualMoveDirection );
         characterController.Move(actualMoveDirection * characterMoveSpeed * Time.deltaTime); //do movement
     }
 }
